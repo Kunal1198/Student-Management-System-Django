1d122ef6e90f7927bda8ee7693fa80be8a39d3a4
Kunal Bhoyar <kvb1198@gmail.com>
2020-04-08 23:59:18 +0530

    Update settings.py

--- ipproject/settings.py
+++ ipproject/settings.py
@@ -131,6 +131,6 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
